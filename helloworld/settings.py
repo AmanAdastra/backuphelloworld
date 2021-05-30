ad9f1af8d6a286850fd3109b8974e5a67818c8ee
unknown <amanayankhan78@gmail.com>
2021-05-30 18:52:24 +0530

    changed

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-&&sq8304+55(#(0^3+$+o3l0qarsk7zm)z42bf&e_#=ii$u4rg'
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "ds3d8j-ehecyt012g_*3jgvgs_$*x^$2%jbe=z#%hb4!g)$v-6")
+SECRET_KEY = 'django-insecure-&&sq8304+55(#(0^3+$+o3l0qarsk7zm)z42bf&e_#=ii$u4rg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
